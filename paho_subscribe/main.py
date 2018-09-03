4a2dae0e988280bbe41f009325af3cdd968e2b54
azwarazuhri <data.azwarazuhri@gmail.com>
2018-09-03 09:39:23 +0700

    update config main.py

--- paho_subscribe/main.py
+++ paho_subscribe/main.py
@@ -12,7 +12,8 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-client.connect("209.97.168.161", 1883, 60)
+client.connect("43.251.98.16",1883,60)
+#client.connect("209.97.168.161", 1883, 60)
 #client.connect("209.97.168.161", 8083, 60)
 client.loop_forever()
 
