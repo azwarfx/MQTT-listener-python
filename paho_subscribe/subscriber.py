3676391a4b3fe495e228e1d4e10044b6045c3d98
azwarazuhri <data.azwarazuhri@gmail.com>
2018-09-03 16:45:30 +0700

    perbaikian script subscribe

--- paho_subscribe/subscriber.py
+++ paho_subscribe/subscriber.py
@@ -11,7 +11,10 @@ def connect():
         # read connection parameters
     global myConnection
     try:
-        myConnection = psycopg2.connect( host="localhost", user="postgres", password="VVGhrzNAtlPPfOi", dbname="alamsutera") 
+        # myConnection = psycopg2.connect( host="localhost", user="postgres", password="VVGhrzNAtlPPfOi", dbname="alamsutera") 
+
+        myConnection = psycopg2.connect( host="localhost", user="alamsutera", password="alamsutera9545", dbname="alamsutera") 
+
         # connect to the PostgreSQL server
         print('Connecting to the PostgreSQL database...')
  
@@ -47,5 +50,42 @@ def insert_data(unique_key, guard_id, shift_id,cluster_id, latitude, longitude, 
         # print('Connecting to the PostgreSQL database...')
 
         # create a cursor
-        cur = myConnection.cursor()
+           cur = myConnection.cursor()
+        # create a new cursor
+        # execute the INSERT statement
+        cur.execute(sql, (unique_key, guard_id, shift_id, datetime.datetime.today(),cluster_id, longitude, latitude, acc))
+        # get the generated id back
+        vendor_id = cur.fetchone()[0]
+        # commit the changes to the database
+        myConnection.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    # finally:
+    #     if conn is not None:
+    #         conn.close()
+ 
+    return vendor_id
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("v1/api/cluster")
+
+def on_message(client, userdata, msg):
+    dataJson = json.loads(msg.payload)
+    # for key, value in dataJson.items():
+    #     print key, value
+    insert_data(dataJson['unique_key'], dataJson['username'], dataJson['shift_id'], dataJson['cluster_code'], dataJson['lat'], dataJson['lng'], dataJson['acc'])
+ 
+if __name__ == '__main__':
+    myConnection = None
+    connect()
+    client = mqtt.Client()
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.connect("209.97.168.161", 1883, 60)
+    client.loop_forever()
+
+
      
